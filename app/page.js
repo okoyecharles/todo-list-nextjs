c0d61f615bdb96cef9605bcc3399a106830908f3
Charles <okoyecharles509@gmail.com>
2023-08-25 18:11:39 +0100

    Confirm all component creations

--- app/page.js
+++ app/page.js
@@ -6,26 +6,26 @@ export default function Home() {
   
     app
     - nav (github link) <C> done
-    - todo container <C>
-      - todo header <C>
-        - logo <C>
-        - search bar <C>
-        - show completed button <F>
-        - create button <F>
-      - todo list <C>
-        - todo <C>
-          - edit marker
-          - checkbox
-          - title
-          - edit button
-          - delete button
-    - create todo modal <F>
-    - todo description modal <F>
-    - modal container <C>
+    - todo container <C> done
+      - todo header <C> done
+        - logo <C> done
+        - search bar <C> done
+        - show completed button <F> done
+        - create button <F> done
+      - todo list <C> done
+        - todo <C> done
+          - edit marker done
+          - checkbox done
+          - title done
+          - edit button done
+          - delete button done
+    - create todo modal <F> done
+    - todo description modal <F> done
+    - modal container <C> done
       - modal title
       - modal content
       - modal buttons
-    - button <C>
+    - button <C> done
 
   */
   return (
