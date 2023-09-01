ade5b85cc900fc8edac250c2626f733f917d14ee
Charles <okoyecharles509@gmail.com>
2023-09-01 08:43:49 +0100

    Install and setup next auth

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,12 @@
+import GoogleProvider from "next-auth/providers/google";
+
+const options = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ]
+}
+
+export default options;
